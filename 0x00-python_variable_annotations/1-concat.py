32e17ee2ae72051157f49ad621c4959aa26689b2
root <you@example.com>
2024-05-02 05:24:13 -0700

    Concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""concat module"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    concat: takes two strings and returns a concatenated string
+    """
+    return str1 + str2
